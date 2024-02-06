a2c1d97d67490c3e89cd58f9313b67ec2fc26db6
aitorru <aitorruiz2m@gmail.com>
2024-02-06 21:26:38 +0100

    change urils opener

--- utils/utils.py
+++ utils/utils.py
@@ -4,11 +4,11 @@ import webbrowser
 
 def open_file(filename):
     if platform.system() == 'Darwin':       # macOS
-        webbrowser.open_new('file://' + os.path.realpath('sociedades.pdf'))
+        webbrowser.open_new('file://' + os.path.realpath(filename))
     elif platform.system() == 'Windows':    # Windows
         try:
             os.startfile(filename)
         except:
-            webbrowser.open_new('file://' + os.path.realpath('sociedades.pdf'))
+            webbrowser.open_new('file://' + os.path.realpath(filename))
     else:                                   # linux variants
-        webbrowser.open_new('file://' + os.path.realpath('sociedades.pdf'))
+        webbrowser.open_new('file://' + os.path.realpath(filename))
